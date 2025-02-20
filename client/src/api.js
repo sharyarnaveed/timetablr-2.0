73d50e8285cb7068e33a4570e283d9dacf6930a7
sharyar <sharyar430@gmail.com>
2025-02-20 14:27:56 +0500

    version updated

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from './router';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://timetable.pharmder.com',
   withCredentials: true // Enable sending credentials by default
 });
 
